f52b7db8f20354cf6088ba6b000a876639b64bde
StefanyVasc <stefany.vasc.sa@gmail.com>
2023-12-12 11:31:29 -0300

    create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import IgniteLogo from "../assets/ignite-logo.svg"
+import styles from "./Header.module.css"
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={IgniteLogo} alt="Logotipo do Ignite" />
+      <strong>Ignite Feed</strong>
+    </header>
+  )
+}
